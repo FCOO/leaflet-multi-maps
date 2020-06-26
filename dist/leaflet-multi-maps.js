8cb12705d63fea86daf42ab2815ac3e7c7abc6d0
Niels Holt <niels@steenbuchholt.dk>
2020-06-26 17:15:40 +0200

    Bug fix

--- dist/leaflet-multi-maps.js
+++ dist/leaflet-multi-maps.js
@@ -81,7 +81,7 @@ NAME  col#1  col#2  col#3  col#4  col#5
 
         //Default options
         options: {
-            VERSION: "2.0.0",
+            VERSION: "2.0.1",
             id     : defaultId,
             border : true,
             local  : false,
